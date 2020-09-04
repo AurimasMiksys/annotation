d106f0cb56098ef96a23904643c2cdc62fb5422d
AurimasMiksys <Aurimas.Miksys@horiondigital.com>
2020-09-04 16:44:32 +0300

    New

--- app.js
+++ app.js
@@ -1,62 +1,32 @@
-class UiSelectors {
-    constructor() {
-        this.listOfCardsContainer = document.querySelector('#list')
-        this.displayDot = document.querySelector('#dot')
-        this.cardCloseButton = document.querySelectorAll('#closecard')
-        this.deleteCardButton = document.querySelector('#deletebutton')
-        this.formInput = document.querySelector('#forminput')
-        this.formSubmitButton = document.querySelector('#formsubmitbuhgjhkjhktton')
-        this.activeDisabledButton = document.querySelector('#activedisabledbutton')
+class CreateCard {
+    constructor(formInput) {
+        this.formInput = formInput
+        
+    }
+    userClicksButton() {
+        const submitButton = document.querySelector('#formsubmitbutton')
+        submitButton.addEventListener('click', this.createNewCardOnUi)
     }
     
-}
-class EventListeners extends UiSelectors {
-    constructor() {
-        super()
-    }
-
-    domContentLoadedWhenUserVisits() {
-        document.addEventListener('DOMContentLoaded', readDots)
-    }
-    openDot() {
-        this.displayDot.addEventListener('click', showOpenDotReadCard)
-    }
-    createDot() {
-        this.displayDot.addEventListener('dblclick', createDotOpenFormToSubmitToCreateNewCard)
-    }
-    submitForm() {
-        this.formSubmitButton.addEventListener('click', this.userSubmitForm.bind(this))
-    }
-    userCloseCard() {
-        this.cardCloseButton.addEventListener('click', userCloseCard)
-    }
-    userDeleteCard() {
-        this.deleteCardButton.addEventListener('click', userDeleteCard)
-    }
-    userActiveSate() {
-        this.activeDisabledButton.addEventListener('click', userClicksToActivateSate)
-    }
-
-}
-
-class UiReadListOfDots {}
-class UiReadListOfCards {}
-class CreateClosedot {}
-class CreateCloseDeleteCard extends EventListeners {
-    constructor() {
-        super()
-        this.submitForm()
-
-
-    }
-    userSubmitForm(event) {
+    createNewCardOnUi(event) {
         event.preventDefault()
-        console.log('form value:', this.formInput.value)
-
+        const listOfCardsContainer = document.querySelector('#list')
+        const newCard = document.createElement('div')
+        newCard.setAttribute('class', 'card border mt-1 rounded-0')
+        newCard.innerHTML = `<div class="card-body d-flex justify-content-end">
+        <button type="submit" class="btn btn-primary btn-sm rounded-0" id="closecard">X</button>
+        </div>   
+        <p class="p-1 text-left">${this.formInput.value}</p>
+        <button type="submit" class="btn btn-primary btn-lg rounded-0" id="deletebutton">Delete</button>
+        </div>`
+        listOfCardsContainer.appendChild(newCard)   
     }
 }
-class StateActiveDisabled {}
 
 
-//Utilities
-const create = new CreateCloseDeleteCard()
+//App
+
+//Create Card
+const card = new CreateCard(document.querySelector('#forminput'))
+
+card.userClicksButton()
