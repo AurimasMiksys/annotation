d4cbd1659d8cc176c4a5e5f2c8d4b176e5dfa875
Audrius Račkauskas <audrius@horiondigital.com>
2020-09-07 19:33:27 +0300

    naujausias kodas

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 class CreateCard {
-    constructor(elements) {
+    constructor(elements, x, y) {
         this.formInput = elements.formInput
         this.submitButton = elements.submitButton
         this.listOfCardsContainer = elements.listOfCardsContainer
-        
+        this.x = y
+        this.y = y
+        this.iid = Date.now()
     }
     userClicksButton() {
         this.submitButton.addEventListener('click', this.createNewCardOnUi.bind(this))
     }
-    
     createNewCardOnUi(event) {
         event.preventDefault()
         const newCard = document.createElement('div')
         newCard.setAttribute('class', 'card border mt-1 rounded-0')
-        newCard.innerHTML = `<div class="card-body d-flex justify-content-end">
+        newCard.innerHTML = `<div class="card-body d-flex justify-content-end" id="${this.iid}" coordinates="${this.x}${this.y}">
         <button type="submit" class="btn btn-primary btn-sm rounded-0" id="closecard">X</button>
-        </div>   
+        </div>
         <p class="p-1 text-left">${this.formInput.value}</p>
         <button type="submit" class="btn btn-primary btn-lg rounded-0" id="deletebutton">Delete</button>
         </div>`
-        this.listOfCardsContainer.appendChild(newCard)   
+        this.listOfCardsContainer.appendChild(newCard)
     }
 }
-
-
+class DeleteCard {
+    constructor() {
+        this.deleteButton = document.querySelectorAll('#deletebutton')
+    }
+    userClicksDeleteButton() {
+        this.deleteButton.addEventListener('click', this.deleteCardOnUi.bind(this))
+    }
+    deleteCardOnUi(event) {
+        console.log('asdasdasdasdasd')
+    }
+}
 //App
-
 //Create Card
 const card = new CreateCard({
     formInput: document.querySelector('#forminput'),
     submitButton: document.querySelector('#formsubmitbutton'),
     listOfCardsContainer: document.querySelector('#list')
-
 })
-
 card.userClicksButton()
+const deletecard = new DeleteCard({
+})
+deletecard.userClicksDeleteButton()
