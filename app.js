f20514ba5d4d2ec29be929e1ae39d57082ec3135
AurimasMiksys <Aurimas.Miksys@horiondigital.com>
2020-09-07 15:37:02 +0300

    Strange error

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 class CreateCard {
-    constructor(elements) {
+    constructor(elements, x, y) {
         this.formInput = elements.formInput
         this.submitButton = elements.submitButton
         this.listOfCardsContainer = elements.listOfCardsContainer
+        this.x = y
+        this.y = y
+        this.iid = Date.now()
         
     }
     userClicksButton() {
@@ -13,13 +16,28 @@ class CreateCard {
         event.preventDefault()
         const newCard = document.createElement('div')
         newCard.setAttribute('class', 'card border mt-1 rounded-0')
-        newCard.innerHTML = `<div class="card-body d-flex justify-content-end">
+        newCard.innerHTML = `<div class="card-body d-flex justify-content-end" id="${this.iid}" coordinates="${this.x}${this.y}">
         <button type="submit" class="btn btn-primary btn-sm rounded-0" id="closecard">X</button>
         </div>   
         <p class="p-1 text-left">${this.formInput.value}</p>
         <button type="submit" class="btn btn-primary btn-lg rounded-0" id="deletebutton">Delete</button>
         </div>`
         this.listOfCardsContainer.appendChild(newCard)   
+    }
+}
+
+class DeleteCard {
+    constructor(deleteButton) {
+        this.deleteButton = deleteButton
+    }
+    userClicksToDeleteCardButton(){
+        this.deleteButton.addEventListener('click', this.whenUserClicksToDeleteCard.bind(this))
+    }
+
+    whenUserClicksToDeleteCard(event) {
+        console.log('hello')
+        // if (this.clickedItem.classList.contains(`'${this.deleteButton}'`)) {
+        //     this.clickedItem.remove()
     }
 }
 
@@ -31,7 +49,14 @@ const card = new CreateCard({
     formInput: document.querySelector('#forminput'),
     submitButton: document.querySelector('#formsubmitbutton'),
     listOfCardsContainer: document.querySelector('#list')
-
+    
 })
 
 card.userClicksButton()
+
+const deleteCard = new DeleteCard({
+    deleteButton: document.querySelector('.btn-lg')
+})
+
+deleteCard.userClicksToDeleteCardButton()
+deleteCard.whenUserClicksToDeleteCard()
